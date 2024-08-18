4e070f8b3f8269947ee9efaf8dd4962cdaa80ea4
gachombamuthiri <amgachomba6@gmail.com>
2024-08-18 23:03:18 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("enter your name:")
+age = input("enter your age:")
+location = input("enter your location;")
+
+print(f"Hello, {name} You are {age} years old and You are from {location}")
